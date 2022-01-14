c299ecbe0d2daf7c6d956160a35807efc272a2f5
andrewhbashorum <you@example.com>
2022-01-14 07:25:55 +0000

    Java docs updated

---
+++ Lab4/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Follower"},{"p":"<Unnamed>","l":"IntegerObj"},{"p":"<Unnamed>","l":"Leader"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Task"}];updateSearchResults();
